d2ec455fdb4a95f571ac9ce0865d9c168b593d0e
Vikas Rana <90509555+VikasRana10@users.noreply.github.com>
2023-10-08 01:42:12 +0530

    Add files via upload

---
+++ MyPortfolio/main.js
@@ -0,0 +1,5 @@
+// Example form submission handling:
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Add your form submission code here
+});
